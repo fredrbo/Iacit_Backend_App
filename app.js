18e1b1ffc9186994e656f2f85ec568ac175b0954
Frederico Rabelo <frederico.rabelo@triasoftware.com>
2023-04-04 11:19:20 -0300

    adicionando cors

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 app.use(express.json());
 
